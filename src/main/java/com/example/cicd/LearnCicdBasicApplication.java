293b50e1622dfd5cdf9efabfe18df9b731128788
Akhlesh Gour <akhlesh95@gmail.com>
2024-10-17 17:12:17 +0530

    initial commit

---
+++ src/main/java/com/example/cicd/LearnCicdBasicApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCicdBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCicdBasicApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
